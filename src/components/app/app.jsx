6e53a080ced839cd834da081add98018edbdd367
NovaPride <novapride27@gmail.com>
2024-06-12 11:50:19 +0300

    refac: routes with routes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import "./app.scss";
 
@@ -6,6 +7,7 @@ import MainPage from "../../pages/main-page/main-page";
 import OurCoffee from "../../pages/our-coffee-page/our-coffee-page";
 import CoffeeDetails from "../../pages/coffee-details-page/coffee-details-page";
 import ForYourPleasure from "../../pages/for-your-pleasure-page/for-your-pleasure-page";
+import Page404 from "../../pages/404-page/404-page";
 
 import { cardsData, bestCardsData } from "../../resources/data/data";
 
@@ -13,16 +15,72 @@ import { cardsData, bestCardsData } from "../../resources/data/data";
 export default class App extends Component {
   render() {
     return (
-    <>
-      {
-        {
-          "/" : <MainPage cards={bestCardsData}/>,
-          "/ourcoffee" : <OurCoffee cards={cardsData}/>,
-          "/coffeedetails" : <CoffeeDetails/>,
-          "/foryourpleasure" : <ForYourPleasure cards={cardsData}/>,
-        }[window.location.pathname]
-      }
-    </>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage cards={bestCardsData}/>}/>
+          <Route path="/ourcoffee" element={<OurCoffee cards={cardsData}/>}/>
+          <Route path="/foryourpleasure" element={<ForYourPleasure cards={cardsData}/>}/>
+          <Route path="/coffeedetails" element={<CoffeeDetails/>}/>
+          <Route path="*" element={<Page404/>}/>
+        </Routes>
+      </Router>
+    // <>
+    //   {
+    //     {
+    //       "/" : <MainPage cards={bestCardsData}/>,
+    //       "/ourcoffee" : <OurCoffee cards={cardsData}/>,
+    //       "/coffeedetails" : <CoffeeDetails/>,
+    //       "/foryourpleasure" : <ForYourPleasure cards={cardsData}/>,
+    //     }[window.location.pathname]
+    //   }
+    // </>
     )
   }
 }
+
+
+
+/*
+import { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+import AppHeader from "../appHeader/AppHeader";
+import Spinner from "../spinner/Spinner";
+
+const CharactersPage = lazy(() => import("../../pages/CharactersPage"))
+const ComicsPage = lazy(() => import("../../pages/ComicsPage"))
+const SingleComicPage = lazy(() => import("../../pages/SingleComicPage"))
+const SingleCharacterPage = lazy(() => import("../../pages/SingleCharacterPage"))
+const Page404 = lazy(() => import("../../pages/404"))
+
+const App = () => { 
+  return (
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <Suspense fallback={<Spinner/>}>
+            <Routes>
+              <Route path="/" element={<CharactersPage/>}/>
+              <Route path="/comics" element={<ComicsPage/>}/>
+              <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+              <Route path="/character/:characterId" element={<SingleCharacterPage/>}/>
+              <Route path="*" element={<Page404/>}/>
+            </Routes>
+          </Suspense>
+        </main>
+      </div>
+    </Router>
+  )
+}
+
+export default App;
+
+/* 
+{
+  {
+    "/" : <CharacterPage/>,
+    "/comics" : <ComicsPage/>,
+  }[window.location.pathname]
+} 
+*/
---
+++ src/pages/404-page/404-page.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+
+export default class Page404 extends Component {
+  render() {
+    
+    return (
+      <>
+        <div>This page doesnt exist</div>
+        <a href="/">Go back</a>
+      </>
+    )
+  }
+}
+
